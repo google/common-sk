5b252a5ba6336020fe558233a175c0bf19fe04b8
Joe Gregorio <jcgregorio@google.com>
2020-05-06 14:15:28 -0400

    clean

--- modules/hintable.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type Hintable = number | boolean | string | any[] | HintableObject;
-export declare type HintableObject = {
-    [key: string]: Hintable;
-};
-//# sourceMappingURL=hintable.d.ts.map
--- modules/human_test.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=human_test.d.ts.map
